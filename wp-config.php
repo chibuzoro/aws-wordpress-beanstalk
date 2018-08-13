a0a79392a87d3e84c069ecdd09b5819ffa5c90e9
Chibuzor Ogbu <chibuzorogbu@gmail.com>
2018-08-13 23:51:26 +0100

    first version

---
+++ wp-config.php
@@ -0,0 +1,25 @@
+<?php
+define('DB_NAME', $_SERVER['RDS_DB_NAME']);
+define('DB_USER', $_SERVER['RDS_USERNAME']);
+define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
+define('DB_HOST', $_SERVER['RDS_HOSTNAME'] . ':' . $_SERVER['RDS_PORT']);
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('AUTH_KEY',         $_SERVER['AUTH_KEY']);
+define('SECURE_AUTH_KEY',  $_SERVER['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY',    $_SERVER['LOGGED_IN_KEY']);
+define('NONCE_KEY',        $_SERVER['NONCE_KEY']);
+define('AUTH_SALT',        $_SERVER['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $_SERVER['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT',   $_SERVER['LOGGED_IN_SALT']);
+define('NONCE_SALT',       $_SERVER['NONCE_SALT']);
+define('WP_HOME', 'https://' . $_SERVER['SERVER_NAME'] . '/');
+define('WP_SITEURL','https://' . $_SERVER['SERVER_NAME'] . '/');
+define('FORCE_SSL_ADMIN', true);
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
+	$_SERVER['HTTPS']='on';
+$table_prefix  = 'wp_';
+define('WP_DEBUG', false);
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
